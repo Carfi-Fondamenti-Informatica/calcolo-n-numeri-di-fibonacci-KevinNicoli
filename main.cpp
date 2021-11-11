6b0e0bef15c82427de871ec3775750a3bb1fa56f
KevinNicoli <91669026+KevinNicoli@users.noreply.github.com>
2021-11-11 12:50:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
-}
+    int n=0, m=0 , l=0, j=1, k=0, i=0;
+    cin >> n;
+    if(n>2) {
+        cout << j << endl;
+        while (m < n - 1) {
+            k = j + l;
+            cout << k << endl;
+            i=j;
+            j= j+l;
+            l=i;
+            m++;
+
+        }
+    }
+
+    return 0;
