2e5d3698e36627b8cee50368cf7e99117df1aa72
KevinNicoli <91669026+KevinNicoli@users.noreply.github.com>
2021-11-11 12:59:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n=0, m=0 , l=0, j=1, k=0, i=0;
     cin >> n;
-    if(n>2) {
+    if(n>=2) {
         cout << j << endl;
         while (m < n - 1) {
             k = j + l;
